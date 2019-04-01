b1e69707c63d1be9f105884fc8c2d09b3517f437
Pankaj <pankaj@SBMAC0305.local>
2019-04-01 15:56:02 +0530

    name changed for json file

--- app.py
+++ app.py
@@ -9,15 +9,29 @@ with open('AVChanges/json_AV-2.13.json') as f:
     data = json.load(f)
 
 components = data['product']['components']
+print(type(components))
+
 
 @app.route('/')
 def home():
     return jsonify (components)
 
 
-@app.route('/<filename>')
-def detail(filename): 
-    
+@app.route('/component')
+def detail():
+    x = []
+    for c in components:
+        for key, value in c.items():
+            if key == 'name':
+                x.append(value)
+
+    return (str(x))
+
+
+@app.route('/<component>')
+def get_comp_attrs():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
