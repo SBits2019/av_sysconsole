9820ba6a3a4c9fa5919dc3865977dbe239121017
Pankaj <pankaj@SBMAC0305.local>
2019-04-01 17:24:52 +0530

    added component files in text

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 app = Flask(__name__)
 
 # load json file
-with open('AVChanges/json_AV-2.13.json') as f:
+with open('avchanges/json_AV-2.13.json') as f:
     data = json.load(f)
 
 components = data['product']['components']
@@ -17,20 +17,26 @@ def home():
     return jsonify (components)
 
 
-@app.route('/component')
-def detail():
+@app.route('/components')
+def list_components():
+    comps = []
+    with open('avchanges/component_files.json') as f:
+        data = json.load(f)
+        for key, value in data.items():
+           comps.append((key, value)) 
+
+        return str(comps)
+
+
+@app.route('/components/<det_component>')
+def get_comp_attrs(det_component):
     x = []
     for c in components:
         for key, value in c.items():
             if key == 'name':
                 x.append(value)
 
-    return (str(x))
-
-
-@app.route('/<component>')
-def get_comp_attrs():
-    pass
+    # return (str(x))
 
 
 if __name__ == '__main__':
