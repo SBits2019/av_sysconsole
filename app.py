46616199d35883a9ec932208f3284d24c9b2b1d0
Pankaj <pankaj@SBMAC0305.local>
2019-04-02 12:14:44 +0530

    app.py changes merged

--- app.py
+++ app.py
@@ -9,69 +9,46 @@ with open('avchanges/json_AV-2.13.json') as f:
     data = json.load(f)
 
 components = data['product']['components']
-<<<<<<< HEAD
-print(len(components))
-=======
-# print(type(components))
->>>>>>> e17f14c837aa69371359b1347630f74989f346d2
 
 
 @app.route('/')
 def home():
     return jsonify(components)
-<<<<<<< HEAD
-=======
 
 
 @app.route('/index')
 def index():
     return render_template('index.html')
->>>>>>> e17f14c837aa69371359b1347630f74989f346d2
 
 
 @app.route('/components')
 def list_components():
-    component_list = []
+    comps = []
 
     with open('avchanges/component_files.json') as f:
         data = json.load(f)
 
-<<<<<<< HEAD
     comps = [(k, v) for k, v in data.items()]
-=======
-    for key, value in data.items():
-        component_list.append((key, value))
->>>>>>> e17f14c837aa69371359b1347630f74989f346d2
 
-    return render_template('components.html', components=component_list)
+    return render_template('components.html', components=comps)
 
 
-<<<<<<< HEAD
-@app.route('/components/<det_component>')
-def get_comp_attrs(det_component):
-    return get_configfiles(det_component)
+@app.route('/components/<detail_component>')
+def get_comp_attrs(detail_component):
+    return render_template('comp_detail.html', component_files=get_configfiles(detail_component))
 
 
 def get_configfiles(config_comp):
     for comp in components:
         for key, value in comp.items():
             if key == 'name' and value == config_comp:
-                return jsonify([c['file_name'] for c in comp['config_files']])
-=======
-@app.route('/components/<detail_component>')
-def get_comp_attrs(detail_component):
-    for comp in components:
-        for key, value in comp.items():
-            if key == 'name' and value == detail_component:
-                return render_template('comp_detail.html', component_files=[c['file_name'] for c in comp['config_files']])
-                #return jsonify([c['file_name'] for c in comp['config_files']])
+                return [c['file_name'] for c in comp['config_files']]
 
-
->>>>>>> e17f14c837aa69371359b1347630f74989f346d2
 
 @app.route('/component_detail')
 def update_configs():
     pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
