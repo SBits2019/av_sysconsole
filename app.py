bf7abb17bb513d9755dc172aa332ed4ea26af0e8
Pankaj <pankaj@SBMAC0305.local>
2019-04-04 11:31:22 +0530

    a lot of changes, for screencast

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify, render_template
 import json
+from helper_tools import helper_tools
 
 app = Flask(__name__)
 
 
 # load json file
-with open('avchanges/json_AV-2.13.json') as f:
+with open('avchanges/2.13_final.json') as f:
     data = json.load(f)
 
 # separate individual components from the json
@@ -13,62 +14,20 @@ components = data['product']['components']
 
 
 @app.route('/')
-def home():
-    return jsonify(components)
-
-
-@app.route('/index')
 def index():
     return render_template('index.html')
 
 
 @app.route('/components')
 def list_components():
-    with open('avchanges/component_files.json') as f:
-        data = json.load(f)
-    # get the configs in each component that have changes
-    changed_comps = [(k, v) for k, v in data.items()]
-    # get all the configs
-    all_files = get_allconfigfiles()
-    # print(all_files)
-
-    return render_template('components.html', changed_components=changed_comps, components=all_files)
+    return render_template('components.html', changed_components=helper_tools.get_comp_dict())
 
 
 @app.route('/components/<detail_component>/<detail_file>')
 def get_file_attrs(detail_component, detail_file):
 
-    with open('avchanges/component_files.json') as f:
-        data = json.load(f)
-    comps = [(k, v) for k, v in data.items()]
-    changed_comps = get_allconfigfiles()
-
-    for c in components:
-        for key, value in c.items():
-            if key == 'name' and value == detail_component:
-                files = c['config_files']
-                for i in files:
-                    if i['file_name'] == detail_file:
-                        return render_template('comp_detail.html',
-                                               attribute_modifications=i['attribute_modifications'],
-                                               file_name=i['file_name'], components=comps,
-                                               changed_components=changed_comps)
-
-
-def get_configfiles(config_comp):
-    for comp in components:
-        for key, value in comp.items():
-            if key == 'name' and value == config_comp:
-                return [c['file_name'] for c in comp['config_files']]
-
-
-def get_allconfigfiles():
-    x = []
-    for comp in components:
-        if 'config_files' in comp:
-            for c in comp['config_files']:
-                x.append(c['file_name'])
-    return x
+    return render_template('comp_detail.html',
+                           attribute_modifications=helper_tools.get_attribute_modifications(detail_component, detail_file), file_name=detail_file, changed_components=helper_tools.get_comp_dict())
 
 
 @app.route('/component_detail')
---
+++ helper_tools/helper_tools.py
@@ -0,0 +1,58 @@
+import json
+
+
+# load json file
+with open('avchanges/2.13_final.json') as f:
+    json_data = json.load(f)
+
+with open('avchanges/component_files.json') as f:
+    comp_all_files = json.load(f)
+
+components = json_data['product']['components']
+
+
+def get_comp_dict():
+    comp_dict = {}
+    for c in components:
+        comp_dict[c['name']] = [(x, True) for x in get_configfiles(c['name'])]
+
+    all_files = get_allconfigfiles()
+
+    comp_dict_new = {}
+
+    for key in comp_dict.keys():
+        for f in all_files:
+            if f[0] == key:
+                for elem in f[1]:
+                    if elem not in [x[0] for x in comp_dict[key]]:
+                        comp_dict[key].append((elem, False))
+            elif f[0] not in comp_dict.keys():
+                comp_dict_new[f[0]] = [(i, False) for i in f[1]]
+
+    comp_dict_final = {**comp_dict, **comp_dict_new}
+    print(comp_dict_final)
+    return comp_dict_final
+
+
+def get_configfiles(config_comp):
+    for comp in components:
+        for key, value in comp.items():
+            if key == 'name' and value == config_comp:
+                return [c['file_name'] for c in comp['config_files']]
+
+
+def get_allconfigfiles():
+    x = []
+    for k, v in comp_all_files.items():
+        x.append((k, v))
+    return x
+
+
+def get_attribute_modifications(comp_name, comp_file_name):
+    for c in components:
+        for key, value in c.items():
+            if key == 'name' and value == comp_name:
+                files = c['config_files']
+                for i in files:
+                    if i['file_name'] == comp_file_name:
+                        return i['attribute_modifications']
